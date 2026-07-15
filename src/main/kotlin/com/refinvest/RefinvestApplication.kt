559e7f47831f520e5a2619eda1906e8fcec147ed
liveforpresent <dailynsteady@gmail.com>
2026-07-16 01:56:41 +0900

    init

---
+++ src/main/kotlin/com/refinvest/RefinvestApplication.kt
@@ -0,0 +1,11 @@
+package com.refinvest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RefinvestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RefinvestApplication>(*args)
+}
---
+++ src/test/kotlin/com/refinvest/RefinvestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.refinvest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefinvestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
